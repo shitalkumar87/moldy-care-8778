3c7be8062a3128a8205de44f6c67b2e78af7595c
shitalkumar87 <shitalsmn@gmail.com>
2022-09-28 21:56:14 +0530

    day-2

---
+++ components/component.js
@@ -0,0 +1,48 @@
+function footer(){
+    return ` <div>
+    <h1 id="help-zara">HELP</h1>
+    <a class="gift-card">SHOP AT ZARA.COM</a><br>
+    <a class="gift-card"  >PRODUCT</a><br>
+    <a class="gift-card">GIFT CARD</a>
+    <p class="gift-card">PAYMENT</p>
+    <p class="gift-card">SHIPPING</p>
+    <p class="gift-card">EXCHANGES AND RETURNS</p>
+    <p class="gift-card">SHOPS AND COMPANY</p>
+    <p class="gift-card">CLOTHES COLLECTION PROGRAMME</p>
+    <p class="gift-card">MY ACCOUNT</p>
+ </div>
+ <div>
+   <h1 id="help-zara">FOLLOW US</h1> 
+   <P class="gift-card">NEWSLETTER</P>
+   <P class="gift-card">INSTAGRAM </P>
+
+   <P class="gift-card">FACEBOOK</P>
+   <P class="gift-card">TWITTER</P>
+   <P class="gift-card">PINTEREST</P>
+   <P class="gift-card">YOUTUBE</P>
+  
+
+ </div>
+ <div>
+   <h1 id="help-zara">COMPANY</h1> 
+   <P class="gift-card">ABOUT US</P>
+   <P class="gift-card">JOIN LIFE</P>
+   <P class="gift-card">OFFICES</P>
+   <P class="gift-card">STORES</P>
+   <P class="gift-card">WORK WITH US</P>
+  
+   
+ </div>
+ <div>
+   <h1 id="help-zara">POLICIES</h1> 
+   <P class="gift-card">PRIVACY POLICY</P>
+   <P class="gift-card">PURCHASE CONDITIONS   </P>
+ 
+   <P class="gift-card">GIFT CARD CONDITIONS </P>
+
+   <P class="gift-card">COOKIES SETTINGS</P>
+
+ </div>`
+}
+
+export default footer
