87c95432fbf9f93ca3bb6e35349986aa57ab4c51
rajanmanupandey <107976711+rajanmanupandey@users.noreply.github.com>
2022-09-30 02:09:16 +0530

    women page createed

---
+++ landingpages/script/sidebar.js
@@ -0,0 +1,6 @@
+hidesidebar = () => {
+  let s = document.querySelector("#sidebar__content-wrap");
+  s.style.display = "none";
+  let r = document.querySelector(".hamburger");
+  r.style.display = "block";
+};
