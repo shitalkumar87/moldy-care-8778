3ba9d926819bf86b54c68929824db5d4cb797553
shitalkumar87 <shitalsmn@gmail.com>
2022-09-30 23:47:40 +0530

    day4

---
+++ script/search.js
@@ -0,0 +1,17 @@
+let submit=document.getElementById("query")
+      submit.onclick=()=>{
+          
+      let box=document.getElementById("Trend_box")
+        if(box.style.display=="none"){
+          box.style.display="block"
+        }
+
+        else{
+          box.style.display="none"
+        }
+      }
+
+      import footer from "../components/component.js";
+
+let Footer=document.getElementById("footer")
+Footer.innerHTML=footer()
