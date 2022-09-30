9fe425fdeef933d65bff4e095a6c404a7a518a72
DeepakPatil26 <deepakpatil2612@gmail.com>
2022-09-30 21:15:55 +0530

    add files

--- footer.js
+++ footer.js
@@ -1,67 +1,59 @@
 function footer() {
-    return `<div id="about">
-      <div id="help">
-        <ul>
-          <li class="head">HELP</li>
-          <li>SHOP AT ZARA.COM</li>
-          <li>PRODUCT</li>
-          <li>PAYMENT</li>
-          <li>SHIPPING</li>
-          <li>EXCHANGING AND RETURNS</li>
-          <li>SHOPS AND COMPANY</li>
-          <li>CLOTHES COLLECTION PROGRAMME</li>
-          <li>MY ACCOUNT</li>
-        </ul>
-      </div>
-  ​
-      <div>
-        <ul>
-          <li class="head">FOLLOW US</li>
-          <li>NEWS LETTER</li>
-          <li>TIKTOK</li>
-          <li>INSTAGRAM</li>
-          <li>FACEBOOK</li>
-          <li>TWITTER</li>
-          <li>PINTEREST</li>
-          <li>YOUTUBE</li>
-        </ul>
-      </div>
-  ​
-      <div>
-        <ul>
-          <li class="head">COMPANY</li>
-          <li>ABOUT US</li>
-          <li>OFFICES</li>
-          <li>STORES</li>
-          <li>WORK WITH US</li>
-        </ul>
-      </div>
-  ​
-      <div>
-        <ul>
-          <li class="head">POLICES</li>
-          <li>PRIVACY POLICY</li>
-          <li>PURCHASE CONDITIONS</li>
-          <li>COOKIES SETTINGS</li>
-        </ul>
-      </div>
-    </div>
-    <hr style="width: 100%; margin:1rem 0" />
-  ​
-    <div id="copy">
-      <h6>INDIA</h6>
-      <div>
-        <p id="last">© ALL RIGHTS RESERVED</p>
-      </div>
-    </div>
-  ​
-    <div id="bottom">
-      <h6>NAME AND ADDRESS OF THE MANUFACTURER:</h6>
-      <h6>INDUSTRIA DE DISEÑO TEXTIL, S.A. (INDITEX, S.A.)</h6>
-      <h6>
-        AVENIDA DE LA DIPUTACIÓN, EDIFICIO INDITEX, 15143, ARTEIXO (A CORUÑA),
-        SPAIN
-      </h6>
-    </div>`
-  }
+  return ` <div id="about">
+  <div id="help">
+    <ul>
+      <li class="head">HELP</li>
+      <li>SHOP AT ZARA.COM</li>
+      <li>PRODUCT</li>
+      <li>PAYMENT</li>
+      <li>SHIPPING</li>
+      <li>EXCHANGING AND RETURNS</li>
+      <li>SHOPS AND COMPANY</li>
+      <li>CLOTHES COLLECTION PROGRAMME</li>
+      <li>MY ACCOUNT</li>
+    </ul>
+  </div>
+  <div>
+    <ul>
+      <li class="head">FOLLOW US</li>
+      <li>NEWS LETTER</li>
+      <li>TIKTOK</li>
+      <li>INSTAGRAM</li>
+      <li>FACEBOOK</li>
+      <li>TWITTER</li>
+      <li>PINTEREST</li>
+      <li>YOUTUBE</li>
+    </ul>
+  </div>
+  <div>
+    <ul>
+      <li class="head">COMPANY</li>
+      <li>ABOUT US</li>
+      <li>OFFICES</li>
+      <li>STORES</li>
+      <li>WORK WITH US</li>
+    </ul>
+  </div>
+  <div>
+    <ul>
+      <li class="head">POLICES</li>
+      <li>PRIVACY POLICY</li>
+      <li>PURCHASE CONDITIONS</li>
+      <li>COOKIES SETTINGS</li>
+    </ul>
+  </div>
+</div>
+<div id="copy">
+  <h6 style="font-weight: 600">INDIA</h6>
+  <div>
+    <p id="last">© ALL RIGHTS RESERVED</p>
+  </div>
+</div>
+<div id="bottom">
+  <p>NAME AND ADDRESS OF THE MANUFACTURER:</p>
+  <p>INDUSTRIA DE DISEÑO TEXTIL, S.A. (INDITEX, S.A.)</p>
+  <p> AVENIDA DE LA DIPUTACIÓN, EDIFICIO INDITEX, 15143, ARTEIXO (A CORUÑA),
+    SPAIN</p>
+</div>`
+}
   export default footer;
--- navbar.js
+++ navbar.js
@@ -1,30 +1,11 @@
-let token = JSON.parse(localStorage.getItem("token"));
-let name = JSON.parse(localStorage.getItem("userName"));
-let change = "LOG IN";
-if (token) {
-  change = "Hi " + name;
-}
 function navbar() {
-  return `<div id="logo">
-    <a href="./index.html">
-        <img src="https://logodownload.org/wp-content/uploads/2014/05/zara-logo-1.png" alt="zaralogo">
-        </a>
-    </div>
-    
-    <div id="ship">
-        <div class="active">SHIPPING</div>
-        <div> > </div>  
-        <div>PAYMENT</div> 
-        <div> > </div>
-        <div>SUMMARY</div>
-     </div>
-    <div id="help">
-    <a href="./login.html">
-        <div >${change}</div>
-        <a/>
-        <a href="./zarahelp.html">
-        <div>HELP</div>
-        <a/>
-    </div>`;
+  return `<div id="navbar">
+  <div id="logo">
+      <a href="./index.html"><img src="https://logodownload.org/wp-content/uploads/2014/05/zara-logo-1.png" alt="zaralogo"></a>
+      </div>
+      <div id="help">
+          <p><a href="./help.html">HELP</a></p>
+      </div>
+  </div>`;
 }
 export default navbar;
